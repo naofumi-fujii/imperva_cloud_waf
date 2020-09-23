93f7cbad00a637dc848c31ed3ab151115eacd2e8
naofumi-fujii <naofumi.fujii@gmail.com>
2020-09-23 15:47:19 +0900

    +  it "does something useful 2" do

--- spec/imperva_cloud_waf_spec.rb
+++ spec/imperva_cloud_waf_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe ImpervaCloudWaf do
       expect(ImpervaCloudWaf.trusted_proxies.any? { |ip| ip === "198.143.33.4" }).to eq(true)
     end
   end
+
+  it "does something useful 2" do
+    expect(false).to eq(true)
+  end
 end
